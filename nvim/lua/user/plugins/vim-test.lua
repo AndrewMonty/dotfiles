f8c2043e7af7835f8bdfa2c76726faa1a042104b
Andrew Monty <andrew1503@gmail.com>
2023-11-09 15:07:14 -0500

    Configure vim-test to use neovim strategy in a vertical split, and run pest tests through sail

--- nvim/lua/user/plugins/vim-test.lua
+++ nvim/lua/user/plugins/vim-test.lua
@@ -1,3 +1,10 @@
+vim.cmd([[
+  let g:test#strategy = "neovim"
+  let g:test#neovim#start_normal = 1
+  let g:test#neovim#term_position = "vert"
+  let g:test#php#pest#executable = './vendor/bin/sail test --compact'
+]])
+
 vim.keymap.set('n', '<Leader>tn', ':TestNearest<CR>')
 vim.keymap.set('n', '<Leader>tf', ':TestFile<CR>')
 vim.keymap.set('n', '<Leader>ts', ':TestSuite<CR>')
