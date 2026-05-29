df38ade95f55d92478355570b566d53ba818373d
Andrew Monty <andrew1503@gmail.com>
2026-05-29 10:45:15 -0400

    customize fyler

--- nvim/plugin/fyler.lua
+++ nvim/plugin/fyler.lua
@@ -3,7 +3,31 @@ vim.pack.add({
 	"https://github.com/A7Lavinraj/fyler.nvim",
 })
 
-require("fyler").setup()
+require("fyler").setup({
+	integrations = {
+		icon = "nvim_web_devicons",
+	},
+	views = {
+		finder = {
+			close_on_select = false,
+			columns = {
+				permission = {
+					enabled = false,
+				},
+				size = {
+					enabled = false,
+				},
+			},
+			win = {
+				kinds = {
+					split_left_most = {
+						width = 48,
+					},
+				},
+			},
+		},
+	},
+})
 
 vim.keymap.set("n", "<leader>b", function()
 	require("fyler").toggle({ kind = "split_left_most" })
